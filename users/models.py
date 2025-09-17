08ec33dbbf46c0f5a34180c18faa6028024bbea9
bashyrov <bashyroov@gmail.com>
2025-09-17 18:46:15 +0200

    Initialize user and position models in users app

--- users/models.py
+++ users/models.py
@@ -1,3 +1,25 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+class Position(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+
+    class Meta:
+        ordering = ["name"]
+
+    def __str__(self):
+        return self.name
+
+
+class Employee(AbstractUser):
+    position = models.ForeignKey(
+        Position,
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True,
+        related_name="workers"
+    )
+
+    def __str__(self):
+        return f"{self.username} ({self.position})" if self.position else self.username
